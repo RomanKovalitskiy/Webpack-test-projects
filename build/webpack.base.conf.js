c72871837a19476a1043beff111672f4545e7a72
roma <r.kovalitskiy@key-g.com>
2019-12-29 22:57:16 +0300

    add plugins and add settings webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/',
+}
 
 module.exports = {
+  externals: {
+    paths: PATHS.src
+  },
   entry: {
-    app: './src/index.js'
+    app: PATHS.src
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist',
+    filename: `${PATHS.assets}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -67,14 +78,27 @@ module.exports = {
           }
         ],
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      }
     ]
-  },
-  devServer: {
-    overlay: true
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: `${PATHS.assets}css/[name].css`,
     }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/img`, to: `${PATHS.assets}/img` },
+      { from: `${PATHS.src}/static`, to: `${PATHS.assets}/static` },
+    ]),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/index.html`,
+      filename: 'index.html',
+    })
   ],
 }
