a5c42ca5c0f9a9fba70ca6dce6c503d37db71979
roma <r.kovalitskiy@key-g.com>
2020-01-08 13:16:09 +0300

    add pritter and airbnb

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './js/common.js'
-import './assets/css/main.css'
-import './assets/scss/main.scss'
+import './js/common.js';
+import './assets/css/main.css';
+import './assets/scss/main.scss';
+import store from './store';
 
-window.Vue = require('vue')
-import store from './store'
+window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/Example.vue').default)
+Vue.component('example-component', require('./components/Example.vue').default);
 
 const app = new Vue({
   store,
-  el: '#app'
-})
+  el: '#app',
+});
